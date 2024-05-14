0c4c6d6538923510c5da4c150bf655b6d1c20e98
ValRodriguez97 <valentina.rodriguezc1@uqvirtual.edu.co>
2024-05-14 17:35:08 -0500

    cambios minimos

--- parqueadero/src/main/java/co/edu/uniquindio/poo/Parqueadero.java
+++ parqueadero/src/main/java/co/edu/uniquindio/poo/Parqueadero.java
@@ -34,6 +34,7 @@ public class Parqueadero {
         if (posicion.getI() >= 0 && posicion.getI() < puestos.length && posicion.getJ() >= 0 && posicion.getJ() < puestos.length){
             puestos[posicion.getI()][posicion.getJ()] = puesto;
         }
+        
     }
 
      public void añadirVehiculo(Vehiculo vehiculo){
@@ -41,3 +42,9 @@ public class Parqueadero {
     }    
 
 }
+
+//Programación basada en eventos, respuesta a un evento, reaccionar a un evento. Permite una capacidad de respuesta nehirada, un manejo sencillo de flujo de datos
+//estabilidad, 
+//Fundamentos de la recursividad ->capacidad de llamarse a si mimsa varias veces, varias funciones, descomponer un problema. Caso base -> principio de la funcion, finalidad de la función
+//Caso recursivo -> la funcion se llama varias veces hasta que encuentra la solución, permite una claridad en el código, código más legible, facilidad, flexibilidad y adaptabilidad
+
--- parqueadero/src/test/java/co/edu/uniquindio/poo/ParqueaderoTest.java
+++ parqueadero/src/test/java/co/edu/uniquindio/poo/ParqueaderoTest.java
@@ -1,7 +1,5 @@
 package co.edu.uniquindio.poo;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
 
 public class ParqueaderoTest {
